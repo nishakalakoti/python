bf938ad84492373296b11fcd78c9b98c78bbec26
nishakalakoti <kalakotijiinisha@gmail.com>
2024-07-13 17:50:58 +0530

    nisha chnge

---
+++ week1/project1.py
@@ -0,0 +1,27 @@
+print("**************practice1*************")
+numbers=[1,2,3,4,5,6,7,8,9,10]
+squre=[x**2 for x in numbers]
+
+my_tuple=[1,2,3,4,5,6,7,8,9,10]
+my_tuple[3]=50
+
+my_tuple=[1,2,3,4,5,6,7,8,9,10]
+my_tuple[3]=50
+my_tuple[5]=25
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+numbers.remove(10)
+print(squre,my_tuple,my_tuple,numbers)
+
+
+print("**************practice2*************")
+
+naturalnumbers={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+print(naturalnumbers)
+a,b,c,d,e,f,g,h,i,j=my_tuple
+print(a,b,c,d,e,f,g,h,i,j,)
+print(a,b,c,d,e)
+
+
+
+
